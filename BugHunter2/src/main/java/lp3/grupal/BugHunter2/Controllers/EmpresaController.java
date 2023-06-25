3a237a985dc41f379133d122e4bfe06653728a63
AnaliaRivarola <80989474+AnaliaRivarola@users.noreply.github.com>
2023-06-25 19:31:52 -0400

    prueba3

---
+++ BugHunter2/src/main/java/lp3/grupal/BugHunter2/Controllers/EmpresaController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lp3.grupal.BugHunter2.Controllers;
+
+/**
+ *
+ * @author anali
+ */
+public class EmpresaController {
+    /*Hola*/
+}
